ab49bfdbc0351da48cf7140cb92a65d6a33a3197
Aalap Shah <aalapshah12297@gmail.com>
2018-01-21 15:26:54 +0530

    Temporary top_view bugfix.

--- top_view/src/top_view_node.cpp
+++ top_view/src/top_view_node.cpp
@@ -13,53 +13,66 @@ cv::Mat birds_image;
 
 void msgRecieved(const sensor_msgs::ImageConstPtr& msg)
 {
-    try
+  try
     {
-        cv::Mat gs=cv_bridge::toCvShare(msg, "bgr8")->image;
-        cv::warpPerspective(gs, birds_image, Z, cv::Size( 640,480 ), 
-            CV_INTER_LINEAR | CV_WARP_INVERSE_MAP | CV_WARP_FILL_OUTLIERS);
+      cv::Mat gs=cv_bridge::toCvShare(msg, "bgr8")->image;
+
+      //cv::namedWindow("IMAGE",1);
+      //cv::namedWindow("original",1);
+      if(!gs.empty())
+      {
+        cv::warpPerspective( gs, birds_image, Z, cv::Size( 1280, 1280 ) ,
+                         CV_INTER_LINEAR | CV_WARP_INVERSE_MAP | CV_WARP_FILL_OUTLIERS ) ;
+        cout<<Z<<endl;
+      }
+        //cv::imshow("IMAGE",birds_image);
+        //cv::imshow("original",gs);
         cv::waitKey(30);
 
     }
     catch (cv_bridge::Exception& e)
     {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+      ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
     }
 }
 
-int main(int argc, char** argv)
-{   
-  ros::init(argc, argv, "top_view_node");
+int main(int argc,char** argv)
+{ 
+  ros::init(argc,argv,"reciever");
+  cout<< argc << "  " << **argv <<endl;
   ros::NodeHandle n;
-  
-  ifstream fs("~/.ros/top_view.txt");
+
+  ifstream fs("/home/bhatti/catkin_ws/top_view.txt");
   char c;
   fs>>c;
-  for (int i = 0; i < 3; ++i)
-  {
+    for (int i = 0; i < 3; ++i)
+    {
       for (int j = 0; j < 3; ++j)
       {
-          fs>>Z.at<double>(i,j);
-          fs>>c;
+        fs>>Z.at<double>(i,j);
+        fs>>c;
       }
-  }
-  fs.close();
-  cout<<Z<<endl;
-  
-  image_transport::ImageTransport   it(n);
-  image_transport::Publisher pub = it.advertise("/top_view", 1);
-  image_transport::Subscriber sub = it.subscribe("/image_topic", 1, msgRecieved);
-
-  ros::Rate loop_rate(30);
-  while(n.ok())
-  {
-    sensor_msgs::ImagePtr msg1;
-    msg1 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", birds_image).toImageMsg();
-    pub.publish(msg1);
-    ros::spinOnce();
-
-    loop_rate.sleep();
     }
+    fs.close();
+  cout<<Z<<endl;
+  image_transport::ImageTransport it(n);
+        image_transport::Publisher pub = it.advertise("/top_view", 1);
+    ros::Rate loop_rate(30);
+    image_transport::Subscriber sub = it.subscribe("/obstacle_free_image",1,msgRecieved);
+    while(n.ok()){
+  
 
-    return 0;
+
+        
+  
+        sensor_msgs::ImagePtr msg1;
+              msg1 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", birds_image).toImageMsg();
+  pub.publish(msg1);
+  ros::spinOnce();
+       // cout<<msg1<<endl;
+
+loop_rate.sleep();
+}
+
+  return 0;
 }
