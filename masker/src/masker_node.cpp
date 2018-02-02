fc821f63bcbe559485bfb69101d0a21551843b0e
Abhishek Yadav <abhishekcms611@gmail.com>
2018-02-02 20:44:07 +0530

    masker for 1600 suerpixels

--- masker/src/masker_node.cpp
+++ masker/src/masker_node.cpp
@@ -22,132 +22,20 @@
 #include <sensor_msgs/image_encodings.h>
 using namespace std;
 using namespace cv;
+int superpixels=1600;
+int N=40;
+int img_size=800;
+
 
 vector <int > splx1;
 vector <int > splx2;
 vector <pair<int,int> > sply1;
 vector <pair<int,int> > sply2;
-static int mask2d[24][24];
+static int mask2d[N][N];
 //int mask[576]={0};
-static int mask[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};    
-static int labels[500*500]={0};    
-void spline(int N, vector<pair<int,int> >& eqn, vector<int>& loop)
-{
-    int i,j,k,n;
-    cout.precision(4);                        //set precision
-    cout.setf(ios::fixed);
-    double x[N],y[N];
-    for(int i=0;i<N;i++){
-        x[i]=eqn[i].first;
-    }
-    for(int i=0;i<N;i++){
-        y[i]=eqn[i].second;
-    }
-    n=3;                                // n is the degree of Polynomial 
-    double X[2*n+1];                        //Array that will store the values of sigma(xi),sigma(xi^2),sigma(xi^3)....sigma(xi^2n)
-    for (i=0;i<2*n+1;i++)
-    {
-        X[i]=0;
-        for (j=0;j<N;j++)
-            X[i]=X[i]+pow(x[j],i);        //consecutive positions of the array will store N,sigma(xi),sigma(xi^2),sigma(xi^3)....sigma(xi^2n)
-    }
-    double B[n+1][n+2],a[n+1];            //B is the Normal matrix(augmented) that will store the equations, 'a' is for value of the final coefficients
-    for (i=0;i<=n;i++)
-        for (j=0;j<=n;j++)
-            B[i][j]=X[i+j];            //Build the Normal matrix by storing the corresponding coefficients at the right positions except the last column of the matrix
-    double Y[n+1];                    //Array to store the values of sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
-    for (i=0;i<n+1;i++)
-    {    
-        Y[i]=0;
-        for (j=0;j<N;j++)
-        Y[i]=Y[i]+pow(x[j],i)*y[j];        //consecutive positions will store sigma(yi),sigma(xi*yi),sigma(xi^2*yi)...sigma(xi^n*yi)
-    }
-    for (i=0;i<=n;i++)
-        B[i][n+1]=Y[i];                //load the values of Y as the last column of B(Normal Matrix but augmented)
-    n=n+1; 
-    for (i=0;i<n;i++)                    //From now Gaussian Elimination starts(can be ignored) to solve the set of linear equations (Pivotisation)
-        for (k=i+1;k<n;k++)
-            if (B[i][i]<B[k][i])
-                for (j=0;j<=n;j++)
-                {
-                    double temp=B[i][j];
-                    B[i][j]=B[k][j];
-                    B[k][j]=temp;
-                }
-    
-    for (i=0;i<n-1;i++)            //loop to perform the gauss elimination
-        for (k=i+1;k<n;k++)
-            {
-                double t=B[k][i]/B[i][i];
-                for (j=0;j<=n;j++)
-                    B[k][j]=B[k][j]-t*B[i][j];    //make the elements below the pivot elements equal to zero or elimnate the variables
-            }
-    for (i=n-1;i>=0;i--)                //back-substitution
-    {                        //x is an array whose values correspond to the values of x,y,z..
-        a[i]=B[i][n];                //make the variable to be calculated equal to the rhs of the last equation
-        for (j=0;j<n;j++)
-            if (j!=i)            //then subtract all the lhs values except the coefficient of the variable whose value                                   is being calculated
-                a[i]=a[i]-B[i][j]*a[j];
-        a[i]=a[i]/B[i][i];            //now finally divide the rhs by the coefficient of the variable to be calculated
-    }
-    for(int k=0;k<loop.size();k++){
-        int j,i;
-        i=loop[k];
-        double jj= a[0] + a[1]*i + a[2]*i*i + a[3]*i*i*i;
-        j=int(jj);
-        if(mask2d[i][j]==0 && j<24){
-            mask2d[i][j]=1;
-        }
-    }
-}
+static int mask[superpixels]={0};
+static int labels[img_size*img_size]={0};    
 
-void splfit(int mask[])
-{
-        splx1.clear();
-        sply1.clear();
-        splx2.clear();
-        sply2.clear();
-
-        for(int i=0;i<24;i++)
-            for(int j=0;j<24;j++)
-                mask2d[i][j]=mask[i*24+j];
-        
-        int N,flag1,flag2;
-        
-        for(int i=0;i<24;i++){
-            flag1=0;
-            for(int j=0;j<12;j++){
-                if(mask2d[i][j]==1){
-                    sply1.push_back(make_pair(i,j));
-                    flag1=1;
-                }
-                if(flag1==1)
-                    break;
-            }
-            if(flag1==0)
-                splx1.push_back(i);
-        }
-        for(int i=0;i<24;i++){
-            flag2=0;
-            for(int j=12;j<24;j++){
-                if(mask2d[i][j]==1){
-                    sply2.push_back(make_pair(i,j));
-                    flag2=1;
-                }
-                if(flag2==1)
-                    break;
-            }
-            if(flag2==0)
-                splx2.push_back(i);
-        }
-        cout << flush;
-        spline(sply1.size(), sply1, splx1);
-        spline(sply2.size(), sply2, splx2);
-
-        for(int i=0;i<24;i++)
-            for(int j=0;j<24;j++)
-                mask[i*24+j]=mask2d[i][j];
-} 
 void labelsCallback(const std_msgs::UInt16MultiArray::ConstPtr& msg)
 {
     for(int i=0;i<500*500;i++)
@@ -208,33 +96,23 @@ int main(int argc,char **argv)
  
     //splfit(mask); // actual function , use only if needed
 
-    int red_sum[676]={0},green_sum[676]={0},blue_sum[676]={0};
-    int n=26;
+    int red_sum[superpixels]={0},green_sum[superpixels]={0},blue_sum[superpixels]={0};
     int k=0;
-    for(int i=0;i<676;i++)
+    for(int i=0;i<superpixels;i++)
     {
-        if(i/n==0 || i%n==0 || i/n==n-1 || i%n==n-1){
-            red_sum[i] =0;
-            green_sum[i] =0;
-            blue_sum[i] =0;
-        }
-        else {
-            red_sum[i] =mask[k]*255;
-            green_sum[i] =mask[k]*255;
-            blue_sum[i] =mask[k]*255;
-            k++;
-        }
-
+            red_sum[i] =mask[i]*255;
+            green_sum[i] =mask[i]*255;
+            blue_sum[i] =mask[i]*255;
     }
     
-    cv::Mat M1(500,500, CV_8UC3, {0,0,0});
-    for (int x=0;x<500;x++)
+    cv::Mat M1(img_size,img_size, CV_8UC3, {0,0,0});
+    for (int x=0;x<img_size;x++)
     {
-        for(int y=0;y<500;y++)
+        for(int y=0;y<img_size;y++)
         {
-                M1.at<cv::Vec3b>(x,y)[0]=blue_sum[labels[x*500 + y ]] ;// b
-                M1.at<cv::Vec3b>(x,y)[1]=green_sum[labels[x*500 + y ]] ;// g
-                M1.at<cv::Vec3b>(x,y)[2]=red_sum[labels[x*500 + y ]] ;// r
+                M1.at<cv::Vec3b>(x,y)[0]=blue_sum[labels[x*img_size + y ]] ;// b
+                M1.at<cv::Vec3b>(x,y)[1]=green_sum[labels[x*img_size + y ]] ;// g
+                M1.at<cv::Vec3b>(x,y)[2]=red_sum[labels[x*img_size + y ]] ;// r
         }
     }
     //cv::namedWindow("video",1);
